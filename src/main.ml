5a5d54ed58cf7dc9a52ff7060064b0d97fd810c7
serban-petrescu <serban.petr@yahoo.com>
2017-10-21 19:13:03 +0300

    Added type definitions (first version).

--- src/main.ml
+++ src/main.ml
@@ -1,1 +1,62 @@
-print_string "Hello world!\n";;
+type primitiveType = IntegerType 
+								 	 | FloatType 
+									 | BooleanType 
+									 | VoidType
+
+type valueType = PrimitiveType of primitiveType 
+						   | ClassName of string 
+							 | BottomType
+
+type field = (valueType * string)
+type fieldList = (field) list
+
+type value = NullValue 
+					 | IntegerValue of int 
+					 | FloatValue of float 
+					 | BooleanValue of bool
+					 | VoidValue
+
+type integerOperator = IntegerAddition 
+									   | IntegerSubtraction 
+										 | IntegerMultiplication 
+										 | IntegerDivision
+
+type floatOperator = FloatAddition 
+								   | FloatSubtraction 
+									 | FloatMultiplication 
+									 | FloatDivision
+
+type comparisonOperator = LowerThan
+												| LowerOrEqualTo
+												| EqualTo
+												| NotEqualTo
+												| GreaterThan
+												| GreaterOrEqualTo
+
+type binaryLogicalOperator = LogicalAnd 
+													 | LogicalOr
+													
+type unaryLogicalOperator = LogicalNot
+
+type expressionBlock = LocalVariableBlock of valueType * string * expression 
+										 | ImmediateBlock of expression
+										
+and expression = Value of value
+							 | Variable of string
+							 | ObjectField of (string * string)
+							 | ExpressionBlock of expressionBlock
+							 | VariableAssignment of (string * expression)
+							 | ObjectFieldAssignment of (string * string * expression)
+							 | Sequence of (expression * expression)
+							 | IfThenElse of (string * expressionBlock * expressionBlock)
+							 | IntegerOperation of (expression * integerOperator * expression)
+							 | FloatOperation of (expression * floatOperator * expression)
+							 | LogicalBinaryOperation of (expression * binaryLogicalOperator * expression)
+							 | LogicalUnaryOperation of (unaryLogicalOperator * expression)
+							 | ObjectInstantiation of (string * (string) list)
+							 | MethodCall of (string * string * (string) list)
+							 | WhileLoop of (string * expressionBlock)
+							 | ComparisonOperation of (expression * comparisonOperator * expression)
+							 | CastExpression of (string * string)
+							 | InstanceOfExpression of (string * string) 
+							  
--- vagrant/scripts/plugins.sh
+++ vagrant/scripts/plugins.sh
@@ -5,5 +5,7 @@ fi
 opam init
 opam install depext
 opam depext conf-m4.1
-opam install merlin
+opam install merlin.2.5.0
 opam install ocp-indent
+opam install user-setup
+opam user-setup install
